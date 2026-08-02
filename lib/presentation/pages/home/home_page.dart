1712028d28e81aca4b6c5c285ef1d92e62f2939e
Rerurate514 <rerurateyuto@gmail.com>
2026-08-02 15:03:31 +0900

    UiPageWrapperの適用

---
+++ lib/presentation/pages/home/home_page.dart
@@ -0,0 +1,14 @@
+import 'package:dereruministic/presentation/widgets/ui_page_wrapper.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class HomePage extends ConsumerWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return UiPageWrapper(
+      child: Container(),
+    );
+  }
+}
