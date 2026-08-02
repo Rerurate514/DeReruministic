91704b7e7fc9308643226df9113b0937fbbd6741
Rerurate514 <rerurateyuto@gmail.com>
2026-08-02 16:58:10 +0900

    playerStateの仮追加

--- lib/domain/value_object/player_state.dart
+++ lib/domain/value_object/player_state.dart
@@ -1,4 +1,4 @@
-import 'package:dereruministic/domain/value_object/id.dart';
+import 'package:dereruministic/domain/entities/game_card.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'player_state.freezed.dart';
@@ -7,7 +7,16 @@ part 'player_state.g.dart';
 @freezed
 sealed class PlayerState with _$PlayerState {
   const factory PlayerState({
-    required Id id,
+    required int hp,
+    required int shield,
+    required int currentCost,
+    required List<GameCard> hand,
+    required List<GameCard> deck,
+    required List<GameCard> graveyard,
+    // required List<BuffDebuff> buffs,
+    required List<String> buffs,
+    required int cardsPlayedThisTurn,
+    required int maxHandSize,
   }) = _PlayerState;
 
   factory PlayerState.fromJson(Map<String, dynamic> json) =>
