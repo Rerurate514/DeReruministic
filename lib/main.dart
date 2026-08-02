a6124ec9d556584076c0fc6e1479db04d63d7fca
Rerurate514 <rerurateyuto@gmail.com>
2026-08-02 14:04:57 +0900

    mainファイルに定義を追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,33 @@
+import 'dart:async';
+
+import 'package:dereruministic/l10n/app_localizations.dart';
+import 'package:dereruministic/presentation/router/router.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
+  runZonedGuarded(
+    () {
+      runApp(const ProviderScope(child: MainApp()));
+    },
+    (e, s) {
+      debugPrint('catch on runZonedGuarded: $e /// $s');
+    },
+  );
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends ConsumerWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+  Widget build(BuildContext context, WidgetRef ref) {
+    final router = ref.watch(routerProvider);
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      routerConfig: router,
     );
   }
 }
