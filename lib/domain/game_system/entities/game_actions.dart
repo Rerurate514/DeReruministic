8632407d4c856a1660924343db22be33e7167887
Rerurate514 <rerurateyuto@gmail.com>
2026-08-04 21:40:12 +0900

    エンティティを使用するのを避け、idのみに変更

--- lib/domain/game_system/entities/game_actions.dart
+++ lib/domain/game_system/entities/game_actions.dart
@@ -1,4 +1,4 @@
-import 'package:dereruministic/domain/card/entities/game_card.dart';
+import 'package:dereruministic/domain/card/value_objects/game_card_instance_id.dart';
 import 'package:dereruministic/domain/game_system/value_objects/game_actions_id.dart';
 import 'package:dereruministic/domain/game_system/value_objects/turn_owner.dart';
 import 'package:dereruministic/domain/player/value_objects/player_id.dart';
@@ -20,20 +20,20 @@ sealed class GameActions with _$GameActions {
   const factory GameActions.playCard({
     required GameActionsId id,
     required PlayerId playerId,
-    required GameCard card,
+    required GameCardInstanceId cardInstanceId,
     PlayerId? targetPlayerId,
   }) = GameActionPlayCard;
 
   const factory GameActions.discardCard({
     required GameActionsId id,
     required PlayerId playerId,
-    required GameCard card,
+    required GameCardInstanceId cardInstanceId,
   }) = GameActionDiscardCard;
 
   const factory GameActions.selectOverflowDiscards({
     required GameActionsId id,
     required PlayerId playerId,
-    required List<GameCard> selectedCards,
+    required List<GameCardInstanceId> selectedCardInstanceIds,
   }) = GameActionSelectOverflowDiscards;
 
   const factory GameActions.turnEnd({
