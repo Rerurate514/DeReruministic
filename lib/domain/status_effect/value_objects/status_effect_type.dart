6d843494665dfb8dbe6604f5668db515da905a12
Rerurate514 <rerurateyuto@gmail.com>
2026-08-04 22:34:53 +0900

    fromJsonを追加

--- lib/domain/status_effect/value_objects/status_effect_type.dart
+++ lib/domain/status_effect/value_objects/status_effect_type.dart
@@ -3,10 +3,14 @@ import 'package:dereruministic/domain/status_effect/value_objects/debuff_types.d
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'status_effect_type.freezed.dart';
+part 'status_effect_type.g.dart';
 
 @freezed
 sealed class StatusEffectType with _$StatusEffectType {
   const factory StatusEffectType.buff(BuffTypes type) = StatusEffectTypeBuff;
   const factory StatusEffectType.debuff(DebuffTypes type) =
       StatusEffectTypeDebuff;
+
+  factory StatusEffectType.fromJson(Map<String, dynamic> json) =>
+      _$StatusEffectTypeFromJson(json);
 }
