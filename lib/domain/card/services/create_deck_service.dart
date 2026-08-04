14ecd6b7e067f8425e4111fecd80dd7cf079a594
Rerurate514 <rerurateyuto@gmail.com>
2026-08-04 13:56:19 +0900

    createDeckServiceを作成

---
+++ lib/domain/card/services/create_deck_service.dart
@@ -0,0 +1,41 @@
+import 'dart:math';
+
+import 'package:dereruministic/domain/card/entities/card_definition.dart';
+import 'package:dereruministic/domain/card/entities/game_card.dart';
+import 'package:dereruministic/domain/card/value_objects/card_definition_id.dart';
+import 'package:dereruministic/domain/card/value_objects/game_card_instance_id.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'create_deck_service.g.dart';
+
+@riverpod
+CreateDeckService createDeckService(Ref ref) {
+  return CreateDeckService();
+}
+
+class CreateDeckService {
+  List<GameCard> execute(
+    List<CardDefinition> cardAllDefs,
+    List<CardDefinitionId> deckRecipe,
+    Random random,
+  ) {
+    final defsById = {
+      for (final def in cardAllDefs) def.cardDefId: def,
+    };
+
+    final cards = deckRecipe.map((cardDefId) {
+      final cardDef = defsById[cardDefId];
+      if (cardDef == null) {
+        throw ArgumentError('Unknown CardDefinitionId: $cardDefId');
+      }
+      return GameCard(
+        instanceId: GameCardInstanceId.generate(),
+        definition: cardDef,
+        currentCost: cardDef.baseCost,
+        enteredHandAtTurn: 0,
+      );
+    }).toList();
+
+    return cards..shuffle(random);
+  }
+}
