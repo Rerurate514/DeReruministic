c7c3f11254a5a86a39868f8cf6895fc6c84a0ac5
Rerurate514 <rerurateyuto@gmail.com>
2026-08-04 13:18:03 +0900

    playerStateにファクトリを追加

--- lib/domain/player/value_objects/player_state.dart
+++ lib/domain/player/value_objects/player_state.dart
@@ -1,4 +1,5 @@
 import 'package:dereruministic/domain/card/entities/game_card.dart';
+import 'package:dereruministic/domain/player/constants/player_constants.dart';
 import 'package:dereruministic/domain/status_effect/value_objects/buff_state.dart';
 import 'package:dereruministic/domain/status_effect/value_objects/debuff_state.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
@@ -13,8 +14,8 @@ sealed class PlayerState with _$PlayerState {
     required int maxHp,
     required int shield,
     required int currentCost,
-    required List<GameCard> hand,
     required List<GameCard> deck,
+    required List<GameCard> hand,
     required List<GameCard> graveyard,
     required List<GameCard> exhausted,
     required List<BuffState> buffs,
@@ -23,6 +24,24 @@ sealed class PlayerState with _$PlayerState {
     required int maxHandSize,
     required int drawCount,
   }) = _PlayerState;
+
+  factory PlayerState.create({required List<GameCard> deck}) {
+    return PlayerState(
+      hp: PlayerConstants.defaultInitialHp,
+      maxHp: PlayerConstants.defaultMaxHp,
+      shield: 0,
+      currentCost: PlayerConstants.defaultInitialCost,
+      deck: deck,
+      hand: [],
+      graveyard: [],
+      exhausted: [],
+      buffs: [],
+      debuffs: [],
+      cardsPlayedThisTurn: 0,
+      maxHandSize: PlayerConstants.defaultMaxHandSize,
+      drawCount: PlayerConstants.defaultDrawCount,
+    );
+  }
 
   factory PlayerState.fromJson(Map<String, dynamic> json) =>
       _$PlayerStateFromJson(json);
