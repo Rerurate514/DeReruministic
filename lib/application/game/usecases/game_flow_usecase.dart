376a48592a8b58f4bee7db17be00c9aba6f41038
Rerurate514 <rerurateyuto@gmail.com>
2026-08-04 21:33:13 +0900

    applyGameStartを作成

--- lib/application/game/usecases/game_flow_usecase.dart
+++ lib/application/game/usecases/game_flow_usecase.dart
@@ -1,23 +1,55 @@
 import 'package:dereruministic/domain/game_system/entities/game_actions.dart';
-import 'package:dereruministic/domain/game_system/services/game_action_apply_service.dart';
+import 'package:dereruministic/domain/game_system/services/game_setup_service.dart';
+import 'package:dereruministic/domain/game_system/value_objects/game_setup_context.dart';
 import 'package:dereruministic/domain/game_system/value_objects/game_state.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'game_flow_usecase.g.dart';
+
+@riverpod
+GameFlowUsecase gameFlowUsecase(Ref ref) {
+  return GameFlowUsecase(gameSetupService: ref.read(gameSetupServiceProvider));
+}
 
 class GameFlowUsecase {
-  const GameFlowUsecase({required this.gameActionApplyService});
+  const GameFlowUsecase({required this.gameSetupService});
 
-  final GameActionApplyService gameActionApplyService;
+  final GameSetupService gameSetupService;
 
-  GameState applyAction(GameState current, GameActions action) {
-    return switch (action) {
-      GameActions.gameStart() => _applyGameStart(current, action),
-      GameActions.playCard() => _applyPlayCard(current, action),
-      GameActions.discardCard() => _applyDiscardCard(current, action),
-      GameActions.selectOverflowDiscards() => _applyOverflowDiscards(
-        current,
-        action,
-      ),
-      GameActions.turnEnd() => _applyTurnEndAndAutoAdvance(current, action),
-      GameActions.surrender() => _applySurrender(current, action),
-    };
+  GameState applyAction({
+    required GameState current,
+    required GameActions action,
+    GameSetupContext? setupContext,
+  }) {
+    switch (action) {
+      case GameActionGameStart():
+        return _applyGameStart(current, action, setupContext!);
+      case GameActionPlayCard():
+        return _applyPlayCard(current, action);
+      case GameActionDiscardCard():
+        return _applyDiscardCard(current, action);
+      case GameActionSelectOverflowDiscards():
+        return _applyOverflowDiscards(
+          current,
+          action,
+        );
+      case GameActionTurnEnd():
+        return _applyTurnEndAndAutoAdvance(current, action);
+      case GameActionSurrender():
+        return _applySurrender(current, action);
+    }
+  }
+
+  GameState _applyGameStart(
+    GameState current,
+    GameActionGameStart action,
+    GameSetupContext context,
+  ) {
+    return gameSetupService.execute(
+      player: context.player,
+      enemy: context.enemy,
+      cardDefs: context.cardDefs,
+      seed: context.seed,
+    );
   }
 }
