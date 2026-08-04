685c5d880ab23f6aa665024dde0c079d59c2445b
Rerurate514 <rerurateyuto@gmail.com>
2026-08-04 14:21:47 +0900

    typoの修正とdeckCountの修正

--- lib/domain/player/value_objects/enemy_state.dart
+++ lib/domain/player/value_objects/enemy_state.dart
@@ -1,5 +1,6 @@
 import 'package:dereruministic/domain/card/entities/game_card.dart';
 import 'package:dereruministic/domain/player/constants/player_constants.dart';
+import 'package:dereruministic/domain/player/entities/player.dart';
 import 'package:dereruministic/domain/status_effect/value_objects/buff_state.dart';
 import 'package:dereruministic/domain/status_effect/value_objects/debuff_state.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
@@ -20,13 +21,13 @@ sealed class EnemyState with _$EnemyState {
     required List<DebuffState> debuffs,
   }) = _EnemyState;
 
-  factory EnemyState.create() {
-    return const EnemyState(
+  factory EnemyState.create({required Player enemy}) {
+    return EnemyState(
       hp: PlayerConstants.defaultInitialHp,
       maxHp: PlayerConstants.defaultMaxHp,
-      shield: PlayerConstants.defaultMaxHp,
+      shield: 0,
       handCount: 0,
-      deckCount: 0,
+      deckCount: enemy.deckRecipe.length,
       graveyard: [],
       buffs: [],
       debuffs: [],
