d07c38a92d66d7efbfdb58cb736c2709868f2cc4
Rerurate514 <rerurateyuto@gmail.com>
2026-08-04 11:39:54 +0900

    Player定数を追加

---
+++ lib/domain/player/constants/player_constants.dart
@@ -0,0 +1,7 @@
+abstract class PlayerConstants {
+  static const int defaultInitialHp = 100;
+  static const int defaultMaxHp = 100;
+  static const int defaultInitialCost = 30;
+  static const int defaultMaxHandSize = 12;
+  static const int defaultDrawCount = 3;
+}
