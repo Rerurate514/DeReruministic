ce876ce358d6afa4726bcc0c2d8ea8acd9d3ed29
Rerurate514 <rerurateyuto@gmail.com>
2026-08-05 17:09:21 +0900

    仮作成

---
+++ lib/domain/game_system/services/card_draw_service.dart
@@ -0,0 +1,7 @@
+import 'package:dereruministic/domain/game_system/value_objects/apply_action_result.dart';
+import 'package:dereruministic/domain/game_system/value_objects/game_state.dart';
+
+class CardDrawService {
+  @override
+  ApplyActionResult execute(GameState state, int amount) {}
+}
