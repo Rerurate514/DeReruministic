724d6d93de3fc56e0b172a07c6ba228b374d9d46
Rerurate514 <rerurateyuto@gmail.com>
2026-08-06 19:10:44 +0900

    defeatProccessServiceを削除

--- lib/domain/game_system/services/flows/turn_end_advanced/card_draw_start_turn_service.dart
+++ lib/domain/game_system/services/flows/turn_end_advanced/card_draw_start_turn_service.dart
@@ -1,6 +1,5 @@
 import 'package:dereruministic/domain/card/services/card_draw_service.dart';
 import 'package:dereruministic/domain/game_system/constants/game_system_constants.dart';
-import 'package:dereruministic/domain/game_system/services/defeat_process_service.dart';
 import 'package:dereruministic/domain/game_system/services/game_proccess_pipeline/turn_process_step.dart';
 import 'package:dereruministic/domain/game_system/value_objects/apply_action_result.dart';
 import 'package:dereruministic/domain/game_system/value_objects/game_state.dart';
@@ -13,18 +12,15 @@ part 'card_draw_start_turn_service.g.dart';
 CardDrawStartTurnService cardDrawStartTurnService(Ref ref) {
   return CardDrawStartTurnService(
     cardDrawService: ref.read(cardDrawServiceProvider),
-    defeatProcessService: ref.read(defeatProcessServiceProvider),
   );
 }
 
 class CardDrawStartTurnService implements TurnProcessStep {
   const CardDrawStartTurnService({
-    required this.defeatProcessService,
     required this.cardDrawService,
   });
 
   final CardDrawService cardDrawService;
-  final DefeatProcessService defeatProcessService;
 
   @override
   ApplyActionResult execute(GameState state) {
@@ -32,17 +28,6 @@ class CardDrawStartTurnService implements TurnProcessStep {
     final targetPlayer = state.players[targetPlayerId];
 
     final amount = _calculateDrawAmount(targetPlayer!);
-
-    final totalAvailableCards =
-        targetPlayer.deck.length + targetPlayer.graveyard.length;
-
-    if (totalAvailableCards < amount) {
-      return defeatProcessService.execute(
-        state,
-        loserPlayerId: targetPlayerId,
-        reason: 'defeat_library_out',
-      );
-    }
 
     return cardDrawService.execute(
       state,
