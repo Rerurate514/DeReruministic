554e18382e57af9fc96fd751dad5992cf9ae5598
Rerurate514 <rerurateyuto@gmail.com>
2026-08-07 00:20:28 +0900

    stepEventExecutorを実装

---
+++ lib/presentation/pages/home/executors/step_event_executor.dart
@@ -0,0 +1,37 @@
+import 'package:dereruministic/domain/game_system/value_objects/game_step_event.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class StepEventExecutor {
+  const StepEventExecutor({required this.ref});
+
+  final WidgetRef ref;
+
+  Future<void> execute(GameStepEvent step) async {
+    switch (step) {
+      case GameStepEventComboReset():
+      case GameStepEventDeckShuffled():
+      case GameStepEventOverflowCheckTriggered():
+      case GameStepEventPhaseChanged():
+      case GameStepEventTurnEndEffectsResolved():
+      case GameStepEventRegenApplied():
+      case GameStepEventCostCalculated():
+      case GameStepEventDrawCalculated():
+      case GameStepEventComboUpdated():
+      case GameStepEventDamageDealt():
+      case GameStepEventReflectDamageApplied():
+      case GameStepEventShieldGained():
+      case GameStepEventShieldCleared():
+      case GameStepEventPoisonApplied():
+      case GameStepEventGuardBoostApplied():
+      case GameStepEventStatusEffectChanged():
+      case GameStepEventCardPlayed():
+      case GameStepEventCardExhausted():
+      case GameStepEventDeckRestored():
+      case GameStepEventCardsDrawn():
+      case GameStepEventCardMovedZone():
+      case GameStepEventTurnOwnerSwitched():
+      case GameStepEventGameStarted():
+      case GameStepEventGameEnded():
+    }
+  }
+}
