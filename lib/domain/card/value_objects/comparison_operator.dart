72e530a9eb4c6225564a7a6b5b386db3232c932f
Rerurate514 <rerurateyuto@gmail.com>
2026-08-09 03:04:26 +0900

    比較関数を共通化

--- lib/domain/card/value_objects/comparison_operator.dart
+++ lib/domain/card/value_objects/comparison_operator.dart
@@ -1,7 +1,19 @@
 enum ComparisonOperator {
   equal,
+  notEqual,
   greaterThan,
   greaterOrEqual,
   lessThan,
   lessOrEqual,
+}
+
+extension ComparisonOperatorEx on ComparisonOperator {
+  bool evaluate(num target, num value) => switch (this) {
+    ComparisonOperator.equal => target == value,
+    ComparisonOperator.notEqual => target != value,
+    ComparisonOperator.greaterThan => target > value,
+    ComparisonOperator.greaterOrEqual => target >= value,
+    ComparisonOperator.lessThan => target < value,
+    ComparisonOperator.lessOrEqual => target <= value,
+  };
 }
