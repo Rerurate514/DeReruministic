3bf01ee6d5632dbdbad2924b87140394efd99d3c
Rerurate514 <rerurateyuto@gmail.com>
2026-08-09 08:45:33 +0900

    引数を修正

--- lib/domain/game_system/services/play_card_validator.dart
+++ lib/domain/game_system/services/play_card_validator.dart
@@ -1,13 +1,18 @@
 import 'package:collection/collection.dart';
-import 'package:dereruministic/domain/game_system/entities/game_actions.dart';
+import 'package:dereruministic/domain/card/value_objects/game_card_instance_id.dart';
 import 'package:dereruministic/domain/game_system/value_objects/action_failure_reason.dart';
 import 'package:dereruministic/domain/game_system/value_objects/battle_phase.dart';
 import 'package:dereruministic/domain/game_system/value_objects/game_state.dart';
 import 'package:dereruministic/domain/game_system/value_objects/validation_result.dart';
+import 'package:dereruministic/domain/player/value_objects/player_id.dart';
 
 class PlayCardValidator {
-  ValidationResult validate(GameState state, GameActionPlayCard action) {
-    final cardUsedPlayer = state.players[action.playerId];
+  ValidationResult validate({
+    required GameState state,
+    required PlayerId cardUsedPlayerId,
+    required GameCardInstanceId usedCardInstanceId,
+  }) {
+    final cardUsedPlayer = state.players[cardUsedPlayerId];
 
     if (cardUsedPlayer == null) {
       return const ValidationResultFailure(
@@ -16,14 +21,14 @@ class PlayCardValidator {
     }
 
     if (state.phase.battlePhase != BattlePhase.mainPhase ||
-        state.phase.turnOwner != action.playerId) {
+        state.phase.turnOwner != cardUsedPlayerId) {
       return const ValidationResultFailure(
         reason: ActionFailureReason.invalidPhase,
       );
     }
 
     final card = cardUsedPlayer.hand.firstWhereOrNull(
-      (card) => card.instanceId == action.cardInstanceId,
+      (card) => card.instanceId == usedCardInstanceId,
     );
 
     if (card == null) {
