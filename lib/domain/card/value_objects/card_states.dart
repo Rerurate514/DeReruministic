c5de89cf4a9bd88e6405901fb5223431c785f044
Rerurate514 <rerurateyuto@gmail.com>
2026-08-09 12:02:33 +0900

    コメント追加

--- lib/domain/card/value_objects/card_states.dart
+++ lib/domain/card/value_objects/card_states.dart
@@ -5,42 +5,53 @@ part 'card_states.g.dart';
 
 @freezed
 sealed class CardStates with _$CardStates {
+  //焼却
   const factory CardStates.exhaust() = CardStateExhaust;
 
+  //停滞
   const factory CardStates.undiscardable() = CardStateUndiscardable;
 
+  //循環
   const factory CardStates.recycle({
     int? count,
   }) = CardStateRecycle;
 
+  //反動
   const factory CardStates.overload({
     required int amount,
   }) = CardStateOverload;
 
+  //潜伏
   const factory CardStates.conceal() = CardStateConceal;
 
+  //保留
   const factory CardStates.retain({
     required int turnThreshold,
     required int costReduction,
   }) = CardStateRetain;
 
+  //刻印
   const factory CardStates.engrave({
     required String subTypeEffect,
   }) = CardStateEngrave;
 
+  //連携
   const factory CardStates.chain({
     required String subTypeEffect,
     required int order,
   }) = CardStateChain;
 
+  //時限
   const factory CardStates.countdown({
     required int turns,
   }) = CardStateCountdown;
 
+  //腐敗
   const factory CardStates.decay({
     required int turns,
   }) = CardStateDecay;
 
+  //感染
   const factory CardStates.infect() = CardStateInfect;
 
   factory CardStates.fromJson(Map<String, dynamic> json) =>
