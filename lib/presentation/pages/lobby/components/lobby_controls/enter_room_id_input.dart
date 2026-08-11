296e7cd236ce470bc9ed75801f69eabbe50ce1dd
Rerurate514 <rerurateyuto@gmail.com>
2026-08-11 20:13:03 +0900

    Uiの修正

--- lib/presentation/pages/lobby/components/lobby_controls/enter_room_id_input.dart
+++ lib/presentation/pages/lobby/components/lobby_controls/enter_room_id_input.dart
@@ -1,6 +1,8 @@
 import 'package:dereruministic/l10n/app_localizations.dart';
 import 'package:dereruministic/presentation/components/app_text_field.dart';
 import 'package:dereruministic/presentation/theme/app_color_scheme.dart';
+import 'package:dereruministic/presentation/widgets/ui_filled_circle.dart';
+import 'package:dereruministic/presentation/widgets/ui_flashing_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -16,12 +18,21 @@ class EnterRoomIdInput extends StatelessWidget {
       crossAxisAlignment: .start,
       spacing: 8,
       children: [
-        Text(
-          l10n.lobby_page_controls_panel_enter_room_id_text,
-          style: GoogleFonts.shareTechMono(
-            color: theme.brandSecondary,
-            fontSize: 16,
-          ),
+        Row(
+          spacing: 8,
+          children: [
+            UiFlashingWidget(
+              color: theme.brandSecondary,
+              child: const UiFilledCircle(),
+            ),
+            Text(
+              l10n.lobby_page_controls_panel_enter_room_id_text,
+              style: GoogleFonts.shareTechMono(
+                color: theme.brandSecondary,
+                fontSize: 16,
+              ),
+            ),
+          ],
         ),
         AppTextField(
           hintText: l10n.lobby_page_controls_panel_enter_room_id_hint_text,
