03793cae18814d8b6d894a3595e9ac3de11e3163
Rerurate514 <rerurateyuto@gmail.com>
2026-08-12 00:03:38 +0900

    provider経由でnameを取得するように変更

--- lib/presentation/pages/battle/components/enemy_state/enemy_state_name.dart
+++ lib/presentation/pages/battle/components/enemy_state/enemy_state_name.dart
@@ -1,18 +1,26 @@
+import 'package:dereruministic/domain/player/entities/player.dart';
+import 'package:dereruministic/l10n/app_localizations.dart';
+import 'package:dereruministic/presentation/pages/battle/providers/enemy_ui_state_provider.dart';
 import 'package:dereruministic/presentation/theme/app_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class EnemyStateName extends ConsumerWidget {
-  const EnemyStateName({required this.name, super.key});
+  const EnemyStateName({required this.enemy, super.key});
 
-  final String name;
+  final Player enemy;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final l10n = AppLocalizations.of(context);
     final theme = context.themePalette;
+    final name = ref.watch(
+      enemyPlayerUiStateProvider(enemy).select((s) => s?.name),
+    );
+
     return Text(
-      name,
+      name ?? l10n.game_state_is_null,
       style: GoogleFonts.poppins(color: theme.textSecondary),
     );
   }
