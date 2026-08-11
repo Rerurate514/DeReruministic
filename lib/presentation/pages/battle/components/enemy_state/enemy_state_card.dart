d1ab4e345393cacb5f52c88579befaf6c0181b8e
Rerurate514 <rerurateyuto@gmail.com>
2026-08-12 00:43:11 +0900

    stateをstatelessに改善

--- lib/presentation/pages/battle/components/enemy_state/enemy_state_card.dart
+++ lib/presentation/pages/battle/components/enemy_state/enemy_state_card.dart
@@ -1,37 +1,25 @@
-import 'package:dereruministic/application/game/state/game_notifier.dart';
 import 'package:dereruministic/domain/player/entities/player.dart';
 import 'package:dereruministic/presentation/components/app_card.dart';
+import 'package:dereruministic/presentation/pages/battle/components/enemy_state/enemy_state_hp.dart';
 import 'package:dereruministic/presentation/pages/battle/components/enemy_state/enemy_state_name.dart';
-import 'package:dereruministic/presentation/pages/battle/state/enemy_player_ui_state.dart';
-import 'package:dereruministic/presentation/widgets/ui_loading_indicator.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class EnemyStateCard extends ConsumerWidget {
+class EnemyStateCard extends StatelessWidget {
   const EnemyStateCard({required this.enemy, super.key});
-
   final Player enemy;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final gameState = ref.watch(gameProvider);
-    if (gameState == null) return const UiLoadingIndicator();
-
-    final enemyState = gameState.players[enemy.id];
-    if (enemyState == null) return const UiLoadingIndicator();
-
-    final enemyUiState = EnemyPlayerUiState.create(
-      player: enemy,
-      playerState: enemyState,
-      isTurn: gameState.phase.turnOwner == enemy.id,
-    );
-
+  Widget build(BuildContext context) {
     return AppCard(
       child: Column(
+        crossAxisAlignment: .stretch,
         children: [
-          EnemyStateName(
-            name: enemyUiState.name,
-          ),
+          EnemyStateName(enemy: enemy),
+          EnemyStateHp(enemy: enemy),
+          // EnemyStateBuffs(enemy: enemy),
+          // EnemyStateDebuffs(enemy: enemy),
+          // EnemyStateCost(enemy: enemy),
+          // EnemyStateShield(enemy: enemy),
         ],
       ),
     );
