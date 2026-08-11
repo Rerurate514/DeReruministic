89362eb78de79481f0fe4b54c59d2a4223bdd67a
Rerurate514 <rerurateyuto@gmail.com>
2026-08-11 22:06:44 +0900

    player情報をpageトップでロード

--- lib/presentation/pages/battle/battle_page.dart
+++ lib/presentation/pages/battle/battle_page.dart
@@ -1,12 +1,32 @@
+import 'package:dereruministic/application/game/state/game_notifier.dart';
+import 'package:dereruministic/domain/player/entities/player.dart';
 import 'package:dereruministic/presentation/pages/battle/components/header/battle_header.dart';
 import 'package:dereruministic/presentation/widgets/ui_page_wrapper.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class BattlePage extends StatelessWidget {
-  const BattlePage({super.key});
+class BattlePage extends HookConsumerWidget {
+  const BattlePage({
+    required this.playerA,
+    required this.playerB,
+    required this.seed,
+    super.key,
+  });
+
+  final Player playerA;
+  final Player playerB;
+  final int seed;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    useEffect(() {
+      WidgetsBinding.instance.addPostFrameCallback((_) async {
+        await ref.read(gameProvider.notifier).startGame(playerA, playerB, seed);
+      });
+      return null;
+    }, []);
+
     return const UiPageWrapper(
       padding: EdgeInsets.all(4),
       child: Column(
