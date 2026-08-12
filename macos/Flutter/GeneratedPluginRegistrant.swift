a8912ef0afb0be50c3bb69acb1037d294156ca47
Rerurate514 <rerurateyuto@gmail.com>
2026-08-13 03:05:23 +0900

    flutter upgrade

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import real_liquid_glass
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  LiquidGlassContainerPlugin.register(with: registry.registrar(forPlugin: "LiquidGlassContainerPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
