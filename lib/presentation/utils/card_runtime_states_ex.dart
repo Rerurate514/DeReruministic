5888252ee78fffc0ab20591fb9463d4ad8bb9462
Rerurate514 <rerurateyuto@gmail.com>
2026-08-14 04:42:28 +0900

    語句に変数を追加

--- lib/presentation/utils/card_runtime_states_ex.dart
+++ lib/presentation/utils/card_runtime_states_ex.dart
@@ -6,27 +6,31 @@ extension CardRuntimeStatesEx on CardRuntimeStates {
     switch (this) {
       case CardRuntimeStateRecycleState(:final maxCount, :final remainingCount):
         {
-          if (maxCount == null)
+          if (maxCount == null) {
             return l10n.battle_page_card_runtime_states_recycle_infinity_text;
-          return l10n.battle_page_card_runtime_states_recycle_text;
+          }
+          return l10n.battle_page_card_runtime_states_recycle_text(
+            remainingCount,
+            maxCount,
+          );
         }
-      case CardRuntimeStateCountdownState(
-        :final initialTurns,
-        :final remainingTurns,
-      ):
+      case CardRuntimeStateCountdownState(:final remainingTurns):
         {
-          return l10n.battle_page_card_runtime_states_countdown_text;
+          return l10n.battle_page_card_runtime_states_countdown_text(
+            remainingTurns,
+          );
         }
-      case CardRuntimeStateDecayState(
-        :final initialTurns,
-        :final remainingTurns,
-      ):
+      case CardRuntimeStateDecayState(:final remainingTurns):
         {
-          return l10n.battle_page_card_runtime_states_decay_text;
+          return l10n.battle_page_card_runtime_states_decay_text(
+            remainingTurns,
+          );
         }
       case CardRuntimeStateRetainState(:final turnsInHand):
         {
-          return l10n.battle_page_card_runtime_states_retain_text;
+          return l10n.battle_page_card_runtime_states_retain_text(
+            turnsInHand,
+          );
         }
     }
   }
