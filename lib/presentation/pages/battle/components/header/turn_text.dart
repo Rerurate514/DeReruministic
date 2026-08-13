d5a7918d4ac248213d035eea9ab0bad644e0b341
Rerurate514 <rerurateyuto@gmail.com>
2026-08-13 16:35:21 +0900

    ターン表示を見やすく

--- lib/presentation/pages/battle/components/header/turn_text.dart
+++ lib/presentation/pages/battle/components/header/turn_text.dart
@@ -1,5 +1,6 @@
 import 'package:dereruministic/application/game/state/game_notifier.dart';
 import 'package:dereruministic/l10n/app_localizations.dart';
+import 'package:dereruministic/presentation/theme/app_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -10,19 +11,26 @@ class TurnNext extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final l10n = AppLocalizations.of(context);
+    final theme = context.themePalette;
 
     final turnCount = ref.watch(gameProvider.select((s) => s?.turnCount));
 
     return Column(
-      crossAxisAlignment: .start,
       children: [
         Text(
           l10n.battle_page_header_turn_text,
-          style: GoogleFonts.poppins(fontSize: 12),
+          style: GoogleFonts.poppins(
+            fontSize: 12,
+            color: theme.brandColor,
+            fontWeight: .bold,
+          ),
         ),
         Text(
           '${turnCount ?? l10n.game_state_is_null}',
-          style: GoogleFonts.poppins(fontSize: 12),
+          style: GoogleFonts.poppins(
+            fontSize: 12,
+            fontWeight: .bold,
+          ),
         ),
       ],
     );
