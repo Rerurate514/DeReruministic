98d1f5d627d9bcbb8776a0af67a44fb46cc76b1c
Rerurate514 <rerurateyuto@gmail.com>
2026-08-14 04:02:17 +0900

    カード詳細のコンポーネントで分離

---
+++ lib/presentation/pages/battle/components/card/game_card_detail_area.dart
@@ -0,0 +1,30 @@
+import 'package:dereruministic/domain/card/entities/game_card.dart';
+import 'package:dereruministic/presentation/theme/app_color_scheme.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class GameCardDetailArea extends StatelessWidget {
+  const GameCardDetailArea({required this.gameCard, super.key});
+
+  final GameCard gameCard;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = context.themePalette;
+    return Padding(
+      padding: const EdgeInsets.all(4),
+      child: Text(
+        gameCard.definition.cardDefId.value,
+        style: GoogleFonts.shareTechMono(
+          color: theme.brandSecondary,
+          shadows: [
+            Shadow(
+              color: theme.brandSecondary,
+              blurRadius: 0.7,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
