7d8ef2ff2a9c3036cfb106430c7c9b09e9549cfd
Rerurate514 <rerurateyuto@gmail.com>
2026-08-14 02:10:20 +0900

    battlePhaseのtextを追加

--- lib/presentation/utils/game_phase_ex.dart
+++ lib/presentation/utils/game_phase_ex.dart
@@ -4,7 +4,21 @@ import 'package:dereruministic/l10n/app_localizations.dart';
 
 extension GamePhaseEx on GamePhase {
   String text(AppLocalizations l10n) {
-    return switch (this.battlePhase) {
+    return switch (battlePhase) {
+      BattlePhase.initialize => l10n.battle_page_phase_initialize,
+      BattlePhase.battleStart => l10n.battle_page_phase_battle_start,
+      BattlePhase.turnStart => l10n.battle_page_phase_turn_start,
+      BattlePhase.mainPhase => l10n.battle_page_phase_main_phase,
+      BattlePhase.turnEnd => l10n.battle_page_phase_turn_end,
+      BattlePhase.battleEnd => l10n.battle_page_phase_turn_end,
+      BattlePhase.selectDiscard => l10n.battle_page_phase_select_discard,
+    };
+  }
+}
+
+extension BattlePhaseEx on BattlePhase {
+  String text(AppLocalizations l10n) {
+    return switch (this) {
       BattlePhase.initialize => l10n.battle_page_phase_initialize,
       BattlePhase.battleStart => l10n.battle_page_phase_battle_start,
       BattlePhase.turnStart => l10n.battle_page_phase_turn_start,
