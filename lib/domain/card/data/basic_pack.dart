08dcf264fcaecf4467be5b894e2ee570ed578e33
Rerurate514 <rerurateyuto@gmail.com>
2026-08-14 22:28:23 +0900

    カード定義を追加

--- lib/domain/card/data/basic_pack.dart
+++ lib/domain/card/data/basic_pack.dart
@@ -2,6 +2,7 @@ import 'package:dereruministic/domain/card/entities/card_definition.dart';
 import 'package:dereruministic/domain/card/value_objects/card_definition_id.dart';
 import 'package:dereruministic/domain/card/value_objects/card_effects.dart';
 import 'package:dereruministic/domain/card/value_objects/card_effects_details.dart';
+import 'package:dereruministic/domain/card/value_objects/card_states.dart';
 import 'package:dereruministic/domain/card/value_objects/card_target_types.dart';
 import 'package:dereruministic/domain/card/value_objects/comparison_operator.dart';
 import 'package:dereruministic/domain/card/value_objects/effect_conditions.dart';
@@ -38,6 +39,42 @@ const List<CardDefinition> basicPack = [
     states: [],
   ),
   CardDefinition(
+    cardDefId: CardDefinitionId(value: 'basic_pack_heavy_swing'),
+    name: '大振り',
+    baseCost: 1,
+    effects: [
+      CardEffectsDetails(
+        cardEffect: CardEffects.damage(
+          amount: 9,
+          target: CardTargetTypes.enemy,
+        ),
+      ),
+    ],
+    states: [
+      CardStates.exhaust(),
+    ],
+  ),
+  CardDefinition(
+    cardDefId: CardDefinitionId(value: 'basic_pack_double_slash'),
+    name: '二段斬り',
+    baseCost: 1,
+    effects: [
+      CardEffectsDetails(
+        cardEffect: CardEffects.damage(
+          amount: 3,
+          target: CardTargetTypes.enemy,
+        ),
+      ),
+      CardEffectsDetails(
+        cardEffect: CardEffects.damage(
+          amount: 3,
+          target: CardTargetTypes.enemy,
+        ),
+      ),
+    ],
+    states: [],
+  ),
+  CardDefinition(
     cardDefId: CardDefinitionId(value: 'basic_pack_defence_stance'),
     name: '防御スタンス',
     baseCost: 1,
@@ -64,6 +101,25 @@ const List<CardDefinition> basicPack = [
       ),
     ],
     states: [],
+  ),
+  CardDefinition(
+    cardDefId: CardDefinitionId(value: 'basic_pack_reinforced_wall'),
+    name: '強化壁',
+    baseCost: 1,
+    effects: [
+      CardEffectsDetails(
+        cardEffect: CardEffects.grantShield(
+          amount: 6,
+          target: CardTargetTypes.self,
+        ),
+      ),
+    ],
+    states: [
+      CardStates.retain(
+        turnThreshold: 1,
+        costReduction: 1,
+      ),
+    ],
   ),
   CardDefinition(
     cardDefId: CardDefinitionId(value: 'basic_pack_first_aid'),
@@ -99,6 +155,22 @@ const List<CardDefinition> basicPack = [
     states: [],
   ),
   CardDefinition(
+    cardDefId: CardDefinitionId(value: 'basic_pack_full_recovery'),
+    name: '完全回復',
+    baseCost: 3,
+    effects: [
+      CardEffectsDetails(
+        cardEffect: CardEffects.heal(
+          amount: 20,
+          target: CardTargetTypes.self,
+        ),
+      ),
+    ],
+    states: [
+      CardStates.exhaust(),
+    ],
+  ),
+  CardDefinition(
     cardDefId: CardDefinitionId(value: 'basic_pack_focus'),
     name: '集中',
     baseCost: 1,
@@ -110,6 +182,21 @@ const List<CardDefinition> basicPack = [
       ),
     ],
     states: [],
+  ),
+  CardDefinition(
+    cardDefId: CardDefinitionId(value: 'basic_pack_insight'),
+    name: '洞察',
+    baseCost: 0,
+    effects: [
+      CardEffectsDetails(
+        cardEffect: CardEffects.draw(
+          amount: 1,
+        ),
+      ),
+    ],
+    states: [
+      CardStates.exhaust(),
+    ],
   ),
   CardDefinition(
     cardDefId: CardDefinitionId(value: 'basic_pack_meditation'),
@@ -145,6 +232,21 @@ const List<CardDefinition> basicPack = [
         cardEffect: CardEffects.applyDebuff(
           debuff: DebuffTypes.poison,
           stacks: 3,
+          target: CardTargetTypes.enemy,
+        ),
+      ),
+    ],
+    states: [],
+  ),
+  CardDefinition(
+    cardDefId: CardDefinitionId(value: 'basic_pack_venomous_needle'),
+    name: '毒針',
+    baseCost: 0,
+    effects: [
+      CardEffectsDetails(
+        cardEffect: CardEffects.applyDebuff(
+          debuff: DebuffTypes.poison,
+          stacks: 2,
           target: CardTargetTypes.enemy,
         ),
       ),
@@ -239,6 +341,21 @@ const List<CardDefinition> basicPack = [
     states: [],
   ),
   CardDefinition(
+    cardDefId: CardDefinitionId(value: 'basic_pack_battle_cry'),
+    name: '雄叫び',
+    baseCost: 1,
+    effects: [
+      CardEffectsDetails(
+        cardEffect: CardEffects.applyBuff(
+          buff: BuffTypes.atkBuff,
+          stacks: 1,
+          target: CardTargetTypes.self,
+        ),
+      ),
+    ],
+    states: [],
+  ),
+  CardDefinition(
     cardDefId: CardDefinitionId(value: 'basic_pack_purifying_blow'),
     name: '浄化の一撃',
     baseCost: 2,
@@ -258,5 +375,145 @@ const List<CardDefinition> basicPack = [
       ),
     ],
     states: [],
+  ),
+  CardDefinition(
+    cardDefId: CardDefinitionId(value: 'basic_pack_cleansing_light'),
+    name: '浄化の光',
+    baseCost: 1,
+    effects: [
+      CardEffectsDetails(
+        cardEffect: CardEffects.removeDebuff(
+          debuff: DebuffTypes.poison,
+          stacks: 99,
+          target: CardTargetTypes.self,
+        ),
+      ),
+      CardEffectsDetails(
+        cardEffect: CardEffects.heal(
+          amount: 3,
+          target: CardTargetTypes.self,
+        ),
+      ),
+    ],
+    states: [],
+  ),
+  CardDefinition(
+    cardDefId: CardDefinitionId(value: 'basic_pack_last_resort'),
+    name: 'ラストリゾート',
+    baseCost: 3,
+    effects: [
+      CardEffectsDetails(
+        cardEffect: CardEffects.damage(
+          amount: 25,
+          target: CardTargetTypes.enemy,
+        ),
+      ),
+    ],
+    states: [
+      CardStates.exhaust(),
+    ],
+  ),
+  CardDefinition(
+    cardDefId: CardDefinitionId(value: 'basic_pack_provoke'),
+    name: '挑発',
+    baseCost: 1,
+    effects: [
+      CardEffectsDetails(
+        cardEffect: CardEffects.applyDebuff(
+          debuff: DebuffTypes.atkDebuff,
+          stacks: 1,
+          target: CardTargetTypes.enemy,
+        ),
+      ),
+    ],
+    states: [],
+  ),
+  CardDefinition(
+    cardDefId: CardDefinitionId(value: 'basic_pack_weakening_strike'),
+    name: '弱体化の一撃',
+    baseCost: 1,
+    effects: [
+      CardEffectsDetails(
+        cardEffect: CardEffects.damage(
+          amount: 4,
+          target: CardTargetTypes.enemy,
+        ),
+      ),
+      CardEffectsDetails(
+        cardEffect: CardEffects.applyDebuff(
+          debuff: DebuffTypes.atkDebuff,
+          stacks: 1,
+          target: CardTargetTypes.enemy,
+        ),
+      ),
+    ],
+    states: [],
+  ),
+  CardDefinition(
+    cardDefId: CardDefinitionId(value: 'basic_pack_forbidden_seal'),
+    name: '封印の証',
+    baseCost: 0,
+    effects: [
+      CardEffectsDetails(
+        cardEffect: CardEffects.applyDebuff(
+          debuff: DebuffTypes.atkDebuff,
+          stacks: 1,
+          target: CardTargetTypes.self,
+        ),
+      ),
+    ],
+    states: [
+      CardStates.undiscardable(),
+    ],
+  ),
+  CardDefinition(
+    cardDefId: CardDefinitionId(value: 'basic_pack_all_out_attack'),
+    name: '全力攻撃',
+    baseCost: 2,
+    effects: [
+      CardEffectsDetails(
+        cardEffect: CardEffects.damage(
+          amount: 16,
+          target: CardTargetTypes.enemy,
+        ),
+      ),
+      CardEffectsDetails(
+        cardEffect: CardEffects.applyDebuff(
+          debuff: DebuffTypes.atkDebuff,
+          stacks: 1,
+          target: CardTargetTypes.self,
+        ),
+      ),
+    ],
+    states: [],
+  ),
+  CardDefinition(
+    cardDefId: CardDefinitionId(value: 'basic_pack_guard_break'),
+    name: 'ガードブレイク',
+    baseCost: 1,
+    effects: [
+      CardEffectsDetails(
+        cardEffect: CardEffects.stealShield(
+          amount: 3,
+        ),
+      ),
+    ],
+    states: [],
+  ),
+  CardDefinition(
+    cardDefId: CardDefinitionId(value: 'basic_pack_overwork'),
+    name: '酷使',
+    baseCost: 0,
+    effects: [
+      CardEffectsDetails(
+        cardEffect: CardEffects.grantCost(
+          amount: 1,
+          target: CardTargetTypes.self,
+        ),
+      ),
+    ],
+    states: [
+      CardStates.exhaust(),
+    ],
   ),
 ];
