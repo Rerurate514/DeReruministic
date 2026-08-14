d182f3b383a63318825cd96f0c7e55d06add31bc
Rerurate514 <rerurateyuto@gmail.com>
2026-08-15 02:56:40 +0900

    ボタンを自分のターンかつmainPhaseの際にのみボタンを有効にするように修正

--- lib/presentation/pages/battle/components/end_turn/end_turn_button.dart
+++ lib/presentation/pages/battle/components/end_turn/end_turn_button.dart
@@ -1,4 +1,6 @@
 import 'package:dereruministic/application/game/state/game_notifier.dart';
+import 'package:dereruministic/application/game/state/step_event_queue_notifier.dart';
+import 'package:dereruministic/domain/game_system/value_objects/game_step_event.dart';
 import 'package:dereruministic/domain/player/value_objects/player_id.dart';
 import 'package:dereruministic/l10n/app_localizations.dart';
 import 'package:dereruministic/presentation/components/app_highlight_transparency_button.dart';
@@ -10,30 +12,44 @@ import 'package:material_symbols_icons/symbols.dart';
 
 class EndTurnButton extends HookConsumerWidget {
   const EndTurnButton({required this.playerId, super.key});
-
   final PlayerId playerId;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final l10n = AppLocalizations.of(context);
-
     final isLoading = useState(false);
-    final turnOwner = ref.watch(gameProvider.select((s) => s?.phase.turnOwner));
+    final canEndTurn = useState(false);
+    final isMounted = useIsMounted();
 
-    final isEnable = turnOwner == playerId && !isLoading.value;
+    ref.listen(
+      stepEventQueueProvider,
+      (prev, next) {
+        if (next.isEmpty) return;
+        final phaseChangedEvent = next.firstOrNull;
+        if (phaseChangedEvent == null) return;
+        if (phaseChangedEvent is! GameStepEventPhaseChanged) return;
+        canEndTurn.value =
+            phaseChangedEvent.phase.battlePhase == .mainPhase &&
+            phaseChangedEvent.phase.turnOwner == playerId;
+      },
+    );
+
+    final isEnabled = canEndTurn.value && !isLoading.value;
 
     return SizedBox(
       width: 140,
       height: 40,
       child: AppHighlightTransparencyButton(
         isBlur: true,
-        onPressed: isEnable
+        onPressed: isEnabled
             ? () async {
                 isLoading.value = true;
                 try {
                   await ref.read(gameProvider.notifier).endTurn();
                 } finally {
-                  isLoading.value = false;
+                  if (isMounted()) {
+                    isLoading.value = false;
+                  }
                 }
               }
             : null,
