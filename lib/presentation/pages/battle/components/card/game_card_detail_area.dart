bfbe8e13005da8992b1bc657c4ad83d3931bfc2d
Rerurate514 <rerurateyuto@gmail.com>
2026-08-14 19:24:24 +0900

    カード説明文を追加

--- lib/presentation/pages/battle/components/card/game_card_detail_area.dart
+++ lib/presentation/pages/battle/components/card/game_card_detail_area.dart
@@ -1,5 +1,7 @@
 import 'package:dereruministic/domain/card/entities/game_card.dart';
+import 'package:dereruministic/l10n/app_localizations.dart';
 import 'package:dereruministic/presentation/theme/app_color_scheme.dart';
+import 'package:dereruministic/presentation/utils/card_effects_details_ex.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -10,20 +12,21 @@ class GameCardDetailArea extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final l10n = AppLocalizations.of(context);
     final theme = context.themePalette;
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 6),
-      child: Text(
-        "ここに説明文",
-        style: GoogleFonts.shareTechMono(
-          color: theme.brandSecondary,
-          shadows: [
-            Shadow(
-              color: theme.brandSecondary,
-              blurRadius: 0.7,
-            ),
-          ],
-        ),
+      child: Column(
+        children: gameCard.definition.effects
+            .map(
+              (effect) => Text(
+                effect.text(l10n),
+                style: GoogleFonts.shareTechMono(
+                  color: theme.brandSecondary,
+                ),
+              ),
+            )
+            .toList(),
       ),
     );
   }
