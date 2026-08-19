e02b19b4b412c0c5042c2022ea065899fa6a0379
Rerurate514 <rerurateyuto@gmail.com>
2026-08-20 03:43:13 +0900

    カード状態のテキストを追加

--- lib/presentation/utils/card_states_ex.dart
+++ lib/presentation/utils/card_states_ex.dart
@@ -1,4 +1,5 @@
 import 'package:dereruministic/domain/card/value_objects/card_states.dart';
+import 'package:dereruministic/l10n/app_localizations.dart';
 import 'package:dereruministic/presentation/theme/app_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:material_symbols_icons/material_symbols_icons.dart';
@@ -33,6 +34,43 @@ extension CardStatesEx on CardStates {
       CardStateCountdown() => theme.stateCountdown,
       CardStateDecay() => theme.stateDecay,
       CardStateInfect() => theme.stateInfect,
+    };
+  }
+
+  String title(AppLocalizations l10n) {
+    return switch (this) {
+      CardStateExhaust() => l10n.battle_page_card_state_system_exhaust_title,
+      CardStateUndiscardable() =>
+        l10n.battle_page_card_state_system_undiscardable_title,
+      CardStateRecycle() => l10n.battle_page_card_state_system_recycle_title,
+      CardStateOverload() => l10n.battle_page_card_state_system_overload_title,
+      CardStateConceal() => l10n.battle_page_card_state_system_conceal_title,
+      CardStateRetain() => l10n.battle_page_card_state_system_retain_title,
+      CardStateEngrave() => l10n.battle_page_card_state_system_engrave_title,
+      CardStateChain() => l10n.battle_page_card_state_system_chain_title,
+      CardStateCountdown() =>
+        l10n.battle_page_card_state_system_countdown_title,
+      CardStateDecay() => l10n.battle_page_card_state_system_decay_title,
+      CardStateInfect() => l10n.battle_page_card_state_system_infect_title,
+    };
+  }
+
+  String details(AppLocalizations l10n) {
+    return switch (this) {
+      CardStateExhaust() => l10n.battle_page_card_state_system_exhaust_details,
+      CardStateUndiscardable() =>
+        l10n.battle_page_card_state_system_undiscardable_details,
+      CardStateRecycle() => l10n.battle_page_card_state_system_recycle_details,
+      CardStateOverload() =>
+        l10n.battle_page_card_state_system_overload_details,
+      CardStateConceal() => l10n.battle_page_card_state_system_conceal_details,
+      CardStateRetain() => l10n.battle_page_card_state_system_retain_details,
+      CardStateEngrave() => l10n.battle_page_card_state_system_engrave_details,
+      CardStateChain() => l10n.battle_page_card_state_system_chain_details,
+      CardStateCountdown() =>
+        l10n.battle_page_card_state_system_countdown_details,
+      CardStateDecay() => l10n.battle_page_card_state_system_decay_details,
+      CardStateInfect() => l10n.battle_page_card_state_system_infect_details,
     };
   }
 }
