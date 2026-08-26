d79bd75a67f636a0cc8d878a3b947268bcfe91c1
Rerurate514 <rerurateyuto@gmail.com>
2026-08-26 15:58:57 +0900

    dialogのデザインを変更

--- lib/presentation/dialogs/simple_ok_dialog.dart
+++ lib/presentation/dialogs/simple_ok_dialog.dart
@@ -1,4 +1,8 @@
 import 'package:dereruministic/l10n/app_localizations.dart';
+import 'package:dereruministic/presentation/components/app_card.dart';
+import 'package:dereruministic/presentation/components/app_highlight_button.dart';
+import 'package:dereruministic/presentation/components/app_highlight_transparency_button.dart';
+import 'package:dereruministic/presentation/widgets/ui_gap.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -11,31 +15,47 @@ Future<void> showSimpleOkDialog({
   await showDialog<void>(
     context: context,
     builder: (context) {
-      return AlertDialog(
-        title: title,
-        actionsAlignment: .center,
-        actions: [
-          TextButton(
-            onPressed: () {
-              Navigator.pop(context);
-              return;
-            },
-            child: Text(
-              l10n.dialogs_simple_ok_dialog_cancel_button_text,
-              style: GoogleFonts.shareTechMono(),
-            ),
+      return Center(
+        child: AppCard(
+          isBlur: true,
+          padding: const EdgeInsets.all(32),
+          child: Column(
+            mainAxisSize: .min,
+            children: [
+              title,
+              const UiGap.m(),
+              Row(
+                spacing: 8,
+                mainAxisSize: .min,
+                mainAxisAlignment: .spaceAround,
+                children: [
+                  AppHighlightTransparencyButton(
+                    width: 120,
+                    onPressed: () {
+                      Navigator.pop(context);
+                      return;
+                    },
+                    child: Text(
+                      l10n.dialogs_simple_ok_dialog_cancel_button_text,
+                      style: GoogleFonts.shareTechMono(),
+                    ),
+                  ),
+                  AppHighlightButton(
+                    width: 120,
+                    onPressed: () {
+                      onOkTapped();
+                      Navigator.pop(context);
+                    },
+                    child: Text(
+                      l10n.dialogs_simple_ok_dialog_ok_button_text,
+                      style: GoogleFonts.shareTechMono(fontWeight: .bold),
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
-          TextButton(
-            onPressed: () {
-              onOkTapped();
-              Navigator.pop(context);
-            },
-            child: Text(
-              l10n.dialogs_simple_ok_dialog_ok_button_text,
-              style: GoogleFonts.shareTechMono(),
-            ),
-          ),
-        ],
+        ),
       );
     },
   );
