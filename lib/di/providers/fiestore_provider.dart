16060ad4c727b5064f8dd92c3877446b1892109a
Rerurate514 <rerurateyuto@gmail.com>
2026-09-02 16:20:59 +0900

    各firebaseのインスタンスプロバイダの作成

--- lib/di/providers/fiestore_provider.dart
+++ lib/di/providers/fiestore_provider.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
+part 'fiestore_provider.g.dart';
+
 @riverpod
 FirebaseFirestore firestoreProvider(Ref ref) {
   return FirebaseFirestore.instance;
---
+++ lib/di/providers/google_sign_in_provider.dart
@@ -0,0 +1,9 @@
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'google_sign_in_provider.g.dart';
+
+@riverpod
+GoogleSignIn googleSignIn(Ref ref) {
+  return GoogleSignIn.instance;
+}
