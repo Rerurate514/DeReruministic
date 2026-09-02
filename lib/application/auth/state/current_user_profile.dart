176ee0973a8c358f82c69011829e2d93feed048c
Rerurate514 <rerurateyuto@gmail.com>
2026-09-02 17:33:53 +0900

    認証状態を保持するproviderの作成

---
+++ lib/application/auth/state/current_user_profile.dart
@@ -0,0 +1,15 @@
+import 'package:dereruministic/application/auth/state/auth_provider.dart';
+import 'package:dereruministic/di/providers/auth/user_repository_provider.dart';
+import 'package:dereruministic/domain/player/entities/player.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'current_user_profile.g.dart';
+
+@riverpod
+Stream<Player?> currentUserProfile(Ref ref) {
+  final user = ref.watch(authProvider).value;
+  if (user == null) return Stream.value(null);
+
+  final repository = ref.watch(userRepositoryProvider);
+  return repository.watchPlayer(user.uid);
+}
