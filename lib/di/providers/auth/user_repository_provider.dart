7fa802920b70b8097bb4fa3819cd517c2dc692f2
Rerurate514 <rerurateyuto@gmail.com>
2026-09-02 17:11:22 +0900

    Create user_repository_provider.dart

---
+++ lib/di/providers/auth/user_repository_provider.dart
@@ -0,0 +1,9 @@
+import 'package:dereruministic/di/providers/core/fiestore_provider.dart';
+import 'package:dereruministic/domain/auth/repositories/i_user_repository.dart';
+import 'package:dereruministic/infrastructure/auth/repositories/user_repository_impl.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+@riverpod
+IUserRepository userRepository(Ref ref) {
+  return UserRepositoryImpl(firestore: ref.watch(firestoreProviderProvider));
+}
