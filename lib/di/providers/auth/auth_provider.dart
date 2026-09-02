09248648b0f3eb0e2778ef010b06d284baf12900
Rerurate514 <rerurateyuto@gmail.com>
2026-09-02 16:52:46 +0900

    di層のディレクトリを整理

--- lib/di/providers/auth/auth_provider.dart
+++ lib/di/providers/auth/auth_provider.dart
@@ -1,6 +1,6 @@
-import 'package:dereruministic/di/providers/fiestore_provider.dart';
-import 'package:dereruministic/di/providers/firebase_auth_provider.dart';
-import 'package:dereruministic/di/providers/google_sign_in_provider.dart';
+import 'package:dereruministic/di/providers/core/fiestore_provider.dart';
+import 'package:dereruministic/di/providers/core/firebase_auth_provider.dart';
+import 'package:dereruministic/di/providers/core/google_sign_in_provider.dart';
 import 'package:dereruministic/domain/auth/repositories/i_auth_repository.dart';
 import 'package:dereruministic/infrastructure/auth/repositories/firebase_auth_repository_impl.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
