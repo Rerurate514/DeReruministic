d975db68d9e608a6b5a2aac44c4c0d928ebe5bf8
Rerurate514 <rerurateyuto@gmail.com>
2026-09-03 00:18:55 +0900

    userrepoを作成

--- lib/domain/auth/repositories/i_user_repository.dart
+++ lib/domain/auth/repositories/i_user_repository.dart
@@ -1,6 +1,5 @@
 import 'package:dereruministic/domain/player/entities/player.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 abstract interface class IUserRepository {
-  Future<void> createOrUpdateUserDoc(User user, Player player);
+  Future<void> save(Player player);
 }
--- lib/infrastructure/auth/repositories/user_repository_impl.dart
+++ lib/infrastructure/auth/repositories/user_repository_impl.dart
@@ -2,15 +2,16 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dereruministic/domain/auth/repositories/i_user_repository.dart';
 import 'package:dereruministic/domain/player/entities/player.dart';
 import 'package:dereruministic/infrastructure/auth/constants/collections.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 class UserRepositoryImpl implements IUserRepository {
   UserRepositoryImpl({required this.firestore});
 
   final FirebaseFirestore firestore;
   @override
-  Future<void> createOrUpdateUserDoc(User user, Player player) async {
-    final userRef = firestore.collection(Collections.users).doc(user.uid);
+  Future<void> save(Player player) async {
+    final userRef = firestore
+        .collection(Collections.users)
+        .doc(player.id.value);
 
     await firestore.runTransaction((transaction) async {
       final snapshot = await transaction.get(userRef);
