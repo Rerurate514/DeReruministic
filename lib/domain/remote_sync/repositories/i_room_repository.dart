e5424a3d49d27c2f4e5100ff53324d8f9672266a
Rerurate514 <rerurateyuto@gmail.com>
2026-09-03 11:48:42 +0900

    RoomRepositoryを修正

--- lib/domain/remote_sync/repositories/i_room_repository.dart
+++ lib/domain/remote_sync/repositories/i_room_repository.dart
@@ -1,17 +1,14 @@
 import 'package:dereruministic/domain/player/value_objects/player_id.dart';
 import 'package:dereruministic/domain/remote_sync/entities/room.dart';
 import 'package:dereruministic/domain/remote_sync/value_objects/room_id.dart';
-import 'package:dereruministic/domain/remote_sync/value_objects/room_status.dart';
+import 'package:dereruministic/domain/remote_sync/value_objects/room_watch_result.dart';
 
 abstract interface class IRoomRepository {
-  Future<void> createRoom(Room room);
-  Future<void> joinRoom({
+  Future<Room> createRoom({required PlayerId hostPlayerId});
+  Future<Room> joinRoom({
     required RoomId roomId,
     required PlayerId guestPlayerId,
   });
-  Stream<Room> watchRoom({required RoomId roomId});
-  Future<void> updateStatus({
-    required RoomId roomId,
-    required RoomStatus status,
-  });
+  Stream<RoomWatchResult> watchRoom({required RoomId roomId});
+  Future<Room> startGame({required RoomId roomId});
 }
