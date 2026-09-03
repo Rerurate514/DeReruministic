01f7af3a62f91615e926afaeb836a99a76b3fd42
Rerurate514 <rerurateyuto@gmail.com>
2026-09-03 11:48:28 +0900

    Create room_watch_result.dart

---
+++ lib/domain/remote_sync/value_objects/room_watch_result.dart
@@ -0,0 +1,19 @@
+import 'package:dereruministic/domain/remote_sync/entities/room.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'room_watch_result.freezed.dart';
+part 'room_watch_result.g.dart';
+
+@freezed
+sealed class RoomWatchResult with _$RoomWatchResult {
+  const RoomWatchResult._();
+
+  const factory RoomWatchResult.available({
+    required Room room,
+  }) = RoomWatchResultAvailable;
+
+  const factory RoomWatchResult.unavailable() = RoomWatchResultUnavailable;
+
+  factory RoomWatchResult.fromJson(Map<String, dynamic> json) =>
+      _$RoomWatchResultFromJson(json);
+}
