bdf3956ba86509e6dcbc9259efce4a20da4daba5
Rerurate514 <rerurateyuto@gmail.com>
2026-09-03 11:22:18 +0900

    generateファクトリを追加

--- lib/domain/remote_sync/value_objects/room_id.dart
+++ lib/domain/remote_sync/value_objects/room_id.dart
@@ -1,4 +1,5 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:uuid/uuid.dart';
 
 part 'room_id.freezed.dart';
 part 'room_id.g.dart';
@@ -6,6 +7,10 @@ part 'room_id.g.dart';
 @freezed
 abstract class RoomId with _$RoomId {
   const factory RoomId({required String value}) = _RoomId;
+
+  factory RoomId.generate() {
+    return RoomId(value: const Uuid().v4());
+  }
 
   factory RoomId.fromJson(Map<String, dynamic> json) => _$RoomIdFromJson(json);
 }
