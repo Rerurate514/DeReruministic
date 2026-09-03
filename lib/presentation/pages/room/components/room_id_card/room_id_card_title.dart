4a9e589f1b4d5e7d13a843780d4b9ef3aa2598bb
Rerurate514 <rerurateyuto@gmail.com>
2026-09-03 16:02:25 +0900

    l10n化

--- lib/presentation/pages/room/components/room_id_card/room_id_card_title.dart
+++ lib/presentation/pages/room/components/room_id_card/room_id_card_title.dart
@@ -1,3 +1,4 @@
+import 'package:dereruministic/l10n/app_localizations.dart';
 import 'package:dereruministic/presentation/theme/app_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -8,13 +9,15 @@ class RoomIdCardTitle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final l10n = AppLocalizations.of(context);
     final theme = context.themePalette;
+
     return Row(
       spacing: 4,
       children: [
         Icon(Symbols.bug_report, color: theme.brandSecondary),
         Text(
-          'SECURE MATCH PROTOCOL',
+          l10n.room_page_room_id_card_title,
           style: GoogleFonts.shareTechMono(color: theme.brandSecondary),
         ),
       ],
--- lib/presentation/pages/room/components/room_id_card/room_id_text.dart
+++ lib/presentation/pages/room/components/room_id_card/room_id_text.dart
@@ -1,4 +1,5 @@
 import 'package:dereruministic/domain/remote_sync/room/value_objects/room_id.dart';
+import 'package:dereruministic/l10n/app_localizations.dart';
 import 'package:dereruministic/presentation/theme/app_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -10,39 +11,51 @@ class RoomIdText extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final l10n = AppLocalizations.of(context);
     final theme = context.themePalette;
 
-    return RichText(
-      text: TextSpan(
-        style: GoogleFonts.blackOpsOne(fontSize: 32),
-        children: [
-          TextSpan(
-            text: '# ',
-            style: TextStyle(
-              color: theme.brandColor,
-              shadows: [
-                Shadow(
-                  color: theme.brandColor.withAlpha(180),
-                  offset: const Offset(2, 2),
-                  blurRadius: 4,
-                ),
-              ],
-            ),
+    return Column(
+      crossAxisAlignment: .start,
+      children: [
+        Text(
+          l10n.room_page_room_id_card_id_text,
+          style: GoogleFonts.shareTechMono(
+            color: theme.textPrimary.withAlpha(100),
           ),
-          TextSpan(
-            text: roomId.value,
-            style: TextStyle(
-              shadows: [
-                Shadow(
-                  color: theme.textSecondary.withAlpha(180),
-                  offset: const Offset(2, 2),
-                  blurRadius: 4,
+        ),
+        RichText(
+          text: TextSpan(
+            style: GoogleFonts.blackOpsOne(fontSize: 32),
+            children: [
+              TextSpan(
+                text: l10n.room_page_room_id_card_id_prefix,
+                style: TextStyle(
+                  color: theme.brandColor,
+                  shadows: [
+                    Shadow(
+                      color: theme.brandColor.withAlpha(180),
+                      offset: const Offset(2, 2),
+                      blurRadius: 4,
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+              TextSpan(
+                text: roomId.value,
+                style: TextStyle(
+                  shadows: [
+                    Shadow(
+                      color: theme.textSecondary.withAlpha(180),
+                      offset: const Offset(2, 2),
+                      blurRadius: 4,
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
