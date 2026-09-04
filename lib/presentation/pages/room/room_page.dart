a06ae736edb48297a64b89af0034d970a4963add
Rerurate514 <rerurateyuto@gmail.com>
2026-09-04 21:00:16 +0900

    hostが退出した際にroomを退出するように変更

--- lib/presentation/pages/room/room_page.dart
+++ lib/presentation/pages/room/room_page.dart
@@ -1,21 +1,43 @@
+import 'package:dereruministic/application/remote_sync/room/state/room_watch_provider.dart';
 import 'package:dereruministic/domain/remote_sync/room/value_objects/room_id.dart';
+import 'package:dereruministic/domain/remote_sync/room/value_objects/room_status.dart';
+import 'package:dereruministic/domain/remote_sync/room/value_objects/room_watch_result.dart';
 import 'package:dereruministic/presentation/components/app_title.dart';
+import 'package:dereruministic/presentation/pages/room/components/game_start_button.dart';
 import 'package:dereruministic/presentation/pages/room/components/leave_room_button.dart';
 import 'package:dereruministic/presentation/pages/room/components/players_card/players_cards_section.dart';
 import 'package:dereruministic/presentation/pages/room/components/room_id_card/room_id_card.dart';
 import 'package:dereruministic/presentation/widgets/ui_gap.dart';
 import 'package:dereruministic/presentation/widgets/ui_page_wrapper.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
-class RoomPage extends StatelessWidget {
+class RoomPage extends ConsumerWidget {
   const RoomPage({required this.roomId, super.key});
 
   final RoomId roomId;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.listen(roomWatchProvider(roomId: roomId), (p, n) {
+      n.whenData((data) {
+        switch (data) {
+          case RoomWatchResultAvailable(:final room):
+            {
+              if (room.status == RoomStatus.closed) {
+                if (context.canPop()) context.pop();
+              }
+            }
+          case RoomWatchResultUnavailable():
+            {}
+        }
+      });
+    });
+
     return UiPageWrapper(
       child: SingleChildScrollView(
+        clipBehavior: Clip.none,
         child: Column(
           crossAxisAlignment: .stretch,
           children: [
@@ -40,7 +62,9 @@ class RoomPage extends StatelessWidget {
               roomId: roomId,
             ),
             const UiGap.m(),
-            const Text('ROOM IDはここ'),
+            GameStartButton(
+              roomId: roomId,
+            ),
           ],
         ),
       ),
