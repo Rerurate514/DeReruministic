8de1dd9800b5de4820e4afff47430075f7d8f042
Rerurate514 <rerurateyuto@gmail.com>
2026-09-06 02:22:02 +0900

    converterの適用

--- lib/domain/game_system/entities/game_actions.dart
+++ lib/domain/game_system/entities/game_actions.dart
@@ -1,8 +1,10 @@
+import 'package:dereruministic/domain/card/converter/game_card_instance_id_converter.dart';
 import 'package:dereruministic/domain/card/value_objects/action_targets.dart';
 import 'package:dereruministic/domain/card/value_objects/game_card_instance_id.dart';
 import 'package:dereruministic/domain/create_deck_recipe/entities/deck_recipe.dart';
 import 'package:dereruministic/domain/game_system/converter/game_actions_id_converter.dart';
 import 'package:dereruministic/domain/game_system/value_objects/game_actions_id.dart';
+import 'package:dereruministic/domain/player/converter/player_id_converter.dart';
 import 'package:dereruministic/domain/player/value_objects/player_id.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
@@ -19,8 +21,8 @@ sealed class GameActions with _$GameActions {
   const factory GameActions.gameStart({
     @GameActionsIdConverter() required GameActionsId id,
     required int actionSequenceNumber,
-    required PlayerId playerAId,
-    required PlayerId playerBId,
+    @PlayerIdConverter() required PlayerId playerAId,
+    @PlayerIdConverter() required PlayerId playerBId,
     required DeckRecipe playerADeckRecipe,
     required DeckRecipe playerBDeckRecipe,
     required int seed,
@@ -29,35 +31,36 @@ sealed class GameActions with _$GameActions {
   const factory GameActions.playCard({
     @GameActionsIdConverter() required GameActionsId id,
     required int actionSequenceNumber,
-    required PlayerId playerId,
-    required GameCardInstanceId cardInstanceId,
+    @PlayerIdConverter() required PlayerId playerId,
+    @GameCardInstanceIdConverter() required GameCardInstanceId cardInstanceId,
     ActionTargets? target,
   }) = GameActionPlayCard;
 
   const factory GameActions.discardCard({
     @GameActionsIdConverter() required GameActionsId id,
     required int actionSequenceNumber,
-    required PlayerId playerId,
-    required GameCardInstanceId cardInstanceId,
+    @PlayerIdConverter() required PlayerId playerId,
+    @GameCardInstanceIdConverter() required GameCardInstanceId cardInstanceId,
   }) = GameActionDiscardCard;
 
   const factory GameActions.selectOverflowDiscards({
     @GameActionsIdConverter() required GameActionsId id,
     required int actionSequenceNumber,
-    required PlayerId playerId,
+    @PlayerIdConverter() required PlayerId playerId,
+    @GameCardInstanceIdListConverter()
     required List<GameCardInstanceId> selectedCardInstanceIds,
   }) = GameActionSelectOverflowDiscards;
 
   const factory GameActions.turnEnd({
     @GameActionsIdConverter() required GameActionsId id,
     required int actionSequenceNumber,
-    required PlayerId playerId,
+    @PlayerIdConverter() required PlayerId playerId,
   }) = GameActionTurnEnd;
 
   const factory GameActions.surrender({
     @GameActionsIdConverter() required GameActionsId id,
     required int actionSequenceNumber,
-    required PlayerId playerId,
+    @PlayerIdConverter() required PlayerId playerId,
   }) = GameActionSurrender;
 
   @override
