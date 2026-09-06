1beeebbe308620c722f717593b1ff11c903fce6c
Rerurate514 <rerurateyuto@gmail.com>
2026-09-06 17:31:51 +0900

    Guestが入るまでボタンのデザインを変更

--- lib/presentation/pages/room/components/game_start_button.dart
+++ lib/presentation/pages/room/components/game_start_button.dart
@@ -1,10 +1,14 @@
 import 'package:animated_text_effects/animated_text_effects.dart';
+import 'package:dereruministic/application/remote_sync/room/state/room_watch_provider.dart';
 import 'package:dereruministic/application/remote_sync/room/usecases/start_game_usecase.dart';
+import 'package:dereruministic/domain/remote_sync/room/entities/room.dart';
 import 'package:dereruministic/domain/remote_sync/room/value_objects/room_id.dart';
+import 'package:dereruministic/domain/remote_sync/room/value_objects/room_watch_result.dart';
 import 'package:dereruministic/l10n/app_localizations.dart';
 import 'package:dereruministic/presentation/components/app_highlight_transparency_button.dart';
 import 'package:dereruministic/presentation/painter/glow_line_painter.dart';
 import 'package:dereruministic/presentation/theme/app_color_scheme.dart';
+import 'package:dereruministic/presentation/widgets/ui_loading_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -15,7 +19,7 @@ class GameStartButton extends ConsumerWidget {
   final RoomId roomId;
 
   Widget _corner({
-    required AppColorScheme theme,
+    required Color color,
     required bool flipX,
     required bool flipY,
   }) {
@@ -54,7 +58,7 @@ class GameStartButton extends ConsumerWidget {
         1,
       ),
       child: CustomPaint(
-        painter: GlowLinePainter(path: path, color: theme.brandSecondary),
+        painter: GlowLinePainter(path: path, color: color),
       ),
     );
   }
@@ -63,6 +67,34 @@ class GameStartButton extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final l10n = AppLocalizations.of(context);
     final theme = context.themePalette;
+
+    final roomData = ref.watch(roomWatchProvider(roomId: roomId));
+
+    return roomData.maybeWhen<Widget>(
+      data: (data) {
+        return switch (data) {
+          RoomWatchResultAvailable(:final room) => _buildContent(
+            ref,
+            l10n,
+            theme,
+            room,
+          ),
+          RoomWatchResultUnavailable() =>
+            throw UnimplementedError(), //TODO(low): Error
+        };
+      },
+      orElse: UiLoadingIndicator.new,
+    );
+  }
+
+  Widget _buildContent(
+    WidgetRef ref,
+    AppLocalizations l10n,
+    AppColorScheme theme,
+    Room room,
+  ) {
+    final isExistGuest = room.guestPlayerId != null;
+    final color = isExistGuest ? theme.brandSecondary : theme.brandTertiary;
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 32),
       child: Stack(
@@ -75,17 +107,19 @@ class GameStartButton extends ConsumerWidget {
             child: AnimatedText(
               l10n.room_page_game_start_button_text,
               repeat: true,
-              effects: const [
-                VHSGlitchEffect(
-                  duration: Duration(milliseconds: 1000),
-                  colorOffset: 0,
-                  jitter: 128,
-                  maxBlur: 2,
-                ),
-              ],
+              effects: isExistGuest
+                  ? const [
+                      VHSGlitchEffect(
+                        duration: Duration(milliseconds: 1000),
+                        colorOffset: 0,
+                        jitter: 128,
+                        maxBlur: 2,
+                      ),
+                    ]
+                  : [],
               style: GoogleFonts.shareTechMono(
                 letterSpacing: 2,
-                color: theme.brandSecondary,
+                color: color,
                 fontWeight: .bold,
               ),
             ),
@@ -93,22 +127,22 @@ class GameStartButton extends ConsumerWidget {
           Positioned(
             top: 0,
             left: 0,
-            child: _corner(theme: theme, flipX: false, flipY: false),
+            child: _corner(color: color, flipX: false, flipY: false),
           ),
           Positioned(
             top: 0,
             right: 0,
-            child: _corner(theme: theme, flipX: true, flipY: false),
+            child: _corner(color: color, flipX: true, flipY: false),
           ),
           Positioned(
             bottom: 0,
             left: 0,
-            child: _corner(theme: theme, flipX: false, flipY: true),
+            child: _corner(color: color, flipX: false, flipY: true),
           ),
           Positioned(
             bottom: 0,
             right: 0,
-            child: _corner(theme: theme, flipX: true, flipY: true),
+            child: _corner(color: color, flipX: true, flipY: true),
           ),
         ],
       ),
