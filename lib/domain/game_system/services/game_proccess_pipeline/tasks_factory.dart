36243d3cb335d45fbf480a1c47aea8cba230856b
Rerurate514 <rerurateyuto@gmail.com>
2026-09-08 20:51:35 +0900

    DotShortHandsを適用

--- lib/domain/game_system/services/game_proccess_pipeline/tasks_factory.dart
+++ lib/domain/game_system/services/game_proccess_pipeline/tasks_factory.dart
@@ -1,30 +1,28 @@
 import 'package:collection/collection.dart';
-import 'package:dereruministic/domain/game_system/value_objects/auto_game_task.dart';
 import 'package:dereruministic/domain/game_system/value_objects/game_task.dart';
-import 'package:dereruministic/domain/game_system/value_objects/interactive_game_task.dart';
 import 'package:dereruministic/domain/player/value_objects/player_id.dart';
 
 class TasksFactory {
   static QueueList<GameTask> gameStart({required PlayerId activePlayerId}) =>
       QueueList.from([
-        const GameTask.auto(AutoGameTask.gameStartDrawCards()),
-        const GameTask.auto(AutoGameTask.advanceToTurnStart()),
-        const GameTask.auto(AutoGameTask.calculateCost()),
-        const GameTask.auto(AutoGameTask.advanceToMainPhase()),
-        GameTask.interactive(
-          InteractiveGameTask.mainPhase(activePlayerId: activePlayerId),
+        const .auto(.gameStartDrawCards()),
+        const .auto(.advanceToTurnStart()),
+        const .auto(.calculateCost()),
+        const .auto(.advanceToMainPhase()),
+        .interactive(
+          .mainPhase(activePlayerId: activePlayerId),
         ),
       ]);
 
   static QueueList<GameTask> turnEndTasks({required PlayerId activePlayerId}) =>
       QueueList.from([
-        const GameTask.auto(AutoGameTask.turnEndPhaseChanged()),
-        const GameTask.auto(AutoGameTask.switchTurnOwner()),
-        const GameTask.auto(AutoGameTask.cardDraw()),
-        const GameTask.auto(AutoGameTask.checkHandLimit()),
-        const GameTask.auto(AutoGameTask.advanceToMainPhase()),
-        GameTask.interactive(
-          InteractiveGameTask.mainPhase(activePlayerId: activePlayerId),
+        const .auto(.turnEndPhaseChanged()),
+        const .auto(.switchTurnOwner()),
+        const .auto(.cardDraw()),
+        const .auto(.checkHandLimit()),
+        const .auto(.advanceToMainPhase()),
+        .interactive(
+          .mainPhase(activePlayerId: activePlayerId),
         ),
       ]);
 }
