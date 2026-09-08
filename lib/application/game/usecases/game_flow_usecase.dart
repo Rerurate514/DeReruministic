95ab3e74c4a451142986182644b56ca02d11bd5c
Rerurate514 <rerurateyuto@gmail.com>
2026-09-08 21:04:46 +0900

    throwしないように変更

--- lib/application/game/usecases/game_flow_usecase.dart
+++ lib/application/game/usecases/game_flow_usecase.dart
@@ -150,7 +150,7 @@ class GameFlowUsecase {
         state,
         steps: steps,
       ),
-      ApplyActionResultFailure() => throw UnimplementedError(),
+      final ApplyActionResultFailure failure => failure,
     };
   }
 }
