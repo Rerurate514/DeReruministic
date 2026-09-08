29c59b0507721cae417ed15d56fb14c3b99d47f5
Rerurate514 <rerurateyuto@gmail.com>
2026-09-09 03:58:57 +0900

    CardState解決のサービスをリファクタ

--- lib/domain/card/services/resolve_play_card_states_service.dart
+++ lib/domain/card/services/resolve_play_card_states_service.dart
@@ -1,12 +1,8 @@
-import 'package:dereruministic/domain/card/entities/game_card.dart';
 import 'package:dereruministic/domain/card/value_objects/card_states.dart';
 import 'package:dereruministic/domain/card/value_objects/game_card_instance_id.dart';
 import 'package:dereruministic/domain/game_system/value_objects/action_failure_reason.dart';
 import 'package:dereruministic/domain/game_system/value_objects/apply_action_result.dart';
-import 'package:dereruministic/domain/game_system/value_objects/card_states_trigger_type.dart';
-import 'package:dereruministic/domain/game_system/value_objects/card_zone.dart';
 import 'package:dereruministic/domain/game_system/value_objects/game_state.dart';
-import 'package:dereruministic/domain/game_system/value_objects/game_step_event.dart';
 import 'package:dereruministic/domain/player/value_objects/player_id.dart';
 import 'package:dereruministic/domain/player/value_objects/player_state.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
@@ -36,23 +32,52 @@ class ResolvePlayCardStatesService {
     }
 
     return switch (cardState) {
-      CardStateExhaust() => _applyExhaust(state, playerId, cardInstanceId),
-      CardStateOverload(:final amount) => _applyOverload(
-        state,
-        player,
-        amount,
+      CardStateExhaust() => _applyCleanup(state, playerId, cardInstanceId),
+      CardStateOverload(:final amount) => _applyCleanup(
+        _applyOverload(
+          state,
+          player,
+          amount,
+        ),
+        playerId,
+        cardInstanceId,
       ),
-      CardStateRecycle() => _applyRecycle(state, playerId, cardInstanceId),
+      CardStateRecycle() => _applyCleanup(
+        _applyRecycle(state, playerId, cardInstanceId),
+        playerId,
+        cardInstanceId,
+      ),
       CardStateConceal() => throw UnimplementedError(),
       CardStateRetain() => throw UnimplementedError(),
       CardStateEngrave() => throw UnimplementedError(),
       CardStateChain() => throw UnimplementedError(),
       CardStateInfect() => throw UnimplementedError(),
-      _ => ApplyActionResult.noSteps(state: state),
+      _ => _applyCleanup(
+        state,
+        playerId,
+        cardInstanceId,
+      ),
     };
   }
 
-  ApplyActionResult _applyOverload(
+  ApplyActionResult _applyCleanup(
+    GameState state,
+    PlayerId playerId,
+    GameCardInstanceId instanceId,
+  ) {
+    final newState = state.pushTask(
+      GameStateTaskPushPos.head,
+      .auto(
+        .cleanupPlayCard(
+          playerId: playerId,
+          cardInstanceId: instanceId,
+        ),
+      ),
+    );
+    return ApplyActionResult.success(state: newState, steps: []);
+  }
+
+  GameState _applyOverload(
     GameState state,
     PlayerState player,
     int amount,
@@ -63,84 +88,15 @@ class ResolvePlayCardStatesService {
     final newState = state.copyWith(
       players: {...state.players, player.id: updatedPlayer},
     );
-    return ApplyActionResult.success(state: newState, steps: []);
+    return newState;
   }
 
-  ApplyActionResult _applyExhaust(
+  GameState _applyRecycle(
     GameState state,
     PlayerId playerId,
     GameCardInstanceId instanceId,
-  ) {
-    final newState = state.moveCardZone(
-      playerId: playerId,
-      cardInstanceId: instanceId,
-      from: CardZone.playArea,
-      to: CardZone.exhausted,
-    );
-
-    final step = GameStepEvent.cardMovedZone(
-      playerId: playerId,
-      cardInstanceIds: [instanceId],
-      zoneFrom: CardZone.playArea,
-      zoneTo: CardZone.exhausted,
-    );
-    return ApplyActionResult.success(state: newState, steps: [step]);
-  }
-
-  ApplyActionResult _applyRecycle(
-    GameState state,
-    PlayerId playerId,
-    GameCardInstanceId instanceId,
-  ) {
-    final decrementedState = state.decrementRecycleCount(
-      playerId: playerId,
-      cardInstanceId: instanceId,
-    );
-
-    final updatedCard = decrementedState.findGameCard(
-      playerId: playerId,
-      cardInstanceId: instanceId,
-    );
-
-    final isRecycleActive = updatedCard?.isRecycleActive ?? false;
-
-    if (isRecycleActive) {
-      final newState = decrementedState.moveCardZone(
-        playerId: playerId,
-        cardInstanceId: instanceId,
-        from: CardZone.playArea,
-        to: CardZone.deck,
-      );
-
-      final step = GameStepEvent.cardMovedZone(
-        playerId: playerId,
-        cardInstanceIds: [instanceId],
-        zoneFrom: CardZone.playArea,
-        zoneTo: CardZone.deck,
-      );
-
-      return ApplyActionResult.success(state: newState, steps: [step]);
-    } else {
-      final recycleRuntime = updatedCard?.recycleRuntime;
-      if (recycleRuntime == null) {
-        return ApplyActionResult.failure(
-          state: state,
-          reason: ActionFailureReason.cardNotFound,
-        );
-      }
-
-      final newState = decrementedState.pushTask(
-        GameStateTaskPushPos.head,
-        .auto(
-          .resolveCardStatesTrigger(
-            playerId: playerId,
-            cardInstanceId: instanceId,
-            triggerType: CardStatesTriggerType.recycleExpired,
-          ),
-        ),
-      );
-
-      return ApplyActionResult.success(state: newState, steps: []);
-    }
-  }
+  ) => state.decrementRecycleCount(
+    playerId: playerId,
+    cardInstanceId: instanceId,
+  );
 }
