ecccfa8f3dae3a0b1808c87c16f60e236b3eae90
Rerurate514 <rerurateyuto@gmail.com>
2026-09-10 17:43:49 +0900

    Overflowのswitcherの作成

---
+++ lib/presentation/pages/battle/components/overflowed_discard_area/overflowed_discard_area_switcher.dart
@@ -0,0 +1,29 @@
+import 'package:dereruministic/presentation/pages/battle/providers/step/displayed_overflow_check_triggered_notifier.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class OverflowedDiscardAreaSwitcher extends ConsumerWidget {
+  const OverflowedDiscardAreaSwitcher({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final overflowCount = ref.watch(displayedOverflowCheckTriggeredProvider);
+    return AnimatedSwitcher(
+      duration: const Duration(milliseconds: 200),
+      transitionBuilder: (child, animation) {
+        final offsetAnimation = Tween<Offset>(
+          begin: const Offset(0, 1),
+          end: Offset.zero,
+        ).animate(CurvedAnimation(parent: animation, curve: Curves.easeInOut));
+
+        return SlideTransition(
+          position: offsetAnimation,
+          child: child,
+        );
+      },
+      child: overflowCount != null
+          ? Text(overflowCount.toString())
+          : const SizedBox.shrink(),
+    );
+  }
+}
