4254f824e4af086ec882f2b0396115e2cbe39a3e
Rerurate514 <rerurateyuto@gmail.com>
2026-09-12 11:00:50 +0900

    DiscardDecideButtonを作成

--- lib/presentation/pages/battle/components/overflowed_discard_area/overflowed_discard_area_container.dart
+++ lib/presentation/pages/battle/components/overflowed_discard_area/overflowed_discard_area_container.dart
@@ -1,5 +1,6 @@
 import 'package:dereruministic/l10n/app_localizations.dart';
 import 'package:dereruministic/presentation/components/app_card.dart';
+import 'package:dereruministic/presentation/pages/battle/components/overflowed_discard_area/discard_decide_button.dart';
 import 'package:dereruministic/presentation/pages/battle/components/overflowed_discard_area/overflow_count_text.dart';
 import 'package:dereruministic/presentation/pages/battle/components/overflowed_discard_area/overflowed_discard_area.dart';
 import 'package:dereruministic/presentation/theme/app_color_scheme.dart';
@@ -19,29 +20,37 @@ class OverflowedDiscardAreaContainer extends StatelessWidget {
       padding: const EdgeInsets.all(8),
       isBlur: true,
       blurSigma: 10,
-      child: Column(
+      child: Stack(
         children: [
-          Row(
-            spacing: 8,
+          Column(
             children: [
-              Icon(
-                Symbols.terminal,
-                size: 18,
-                color: theme.textPrimary.withAlpha(100),
+              Row(
+                spacing: 8,
+                children: [
+                  Icon(
+                    Symbols.terminal,
+                    size: 18,
+                    color: theme.textPrimary.withAlpha(100),
+                  ),
+                  Text(
+                    l10n.battle_page_discard_overflowed_card,
+                    style: GoogleFonts.shareTechMono(
+                      letterSpacing: 2,
+                      color: theme.textPrimary.withAlpha(100),
+                    ),
+                  ),
+                ],
               ),
-              Text(
-                l10n.battle_page_discard_overflowed_card,
-                style: GoogleFonts.shareTechMono(
-                  letterSpacing: 2,
-                  color: theme.textPrimary.withAlpha(100),
-                ),
+              const Divider(),
+              const OverflowCountText(),
+              const Expanded(
+                child: OverflowedDiscardArea(),
               ),
             ],
           ),
-          const Divider(),
-          const OverflowCountText(),
-          const Expanded(
-            child: OverflowedDiscardArea(),
+          const Align(
+            alignment: .bottomRight,
+            child: DiscardDecideButton(),
           ),
         ],
       ),
