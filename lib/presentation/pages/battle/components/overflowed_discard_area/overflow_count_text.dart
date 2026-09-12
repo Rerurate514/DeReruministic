a4549cdf2e2b0ac3ef6b31681c33331861a8eb7a
Rerurate514 <rerurateyuto@gmail.com>
2026-09-12 10:44:22 +0900

    l10n化

--- lib/presentation/pages/battle/components/overflowed_discard_area/overflow_count_text.dart
+++ lib/presentation/pages/battle/components/overflowed_discard_area/overflow_count_text.dart
@@ -1,13 +1,17 @@
+import 'package:dereruministic/l10n/app_localizations.dart';
 import 'package:dereruministic/presentation/pages/battle/providers/select_discard_cards_notifier.dart';
 import 'package:dereruministic/presentation/pages/battle/providers/step/displayed_overflow_check_triggered_notifier.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class OverflowCountText extends ConsumerWidget {
   const OverflowCountText({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final l10n = AppLocalizations.of(context);
+
     final overflowCount = ref.watch(displayedOverflowCheckTriggeredProvider);
     final currentSelectedCount = ref.watch(
       selectDiscardCardsProvider.select((s) => s.length),
@@ -15,6 +19,9 @@ class OverflowCountText extends ConsumerWidget {
 
     if (overflowCount == null) return const SizedBox.shrink();
 
-    return Text('残り: $currentSelectedCount / $overflowCount');
+    return Text(
+      l10n.battle_page_overflow_count(currentSelectedCount, overflowCount),
+      style: GoogleFonts.shareTechMono(),
+    );
   }
 }
