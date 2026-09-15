5a6efed2bdd3d20b4e3ea9b88ecc16121dcdf5e0
Rerurate514 <rerurateyuto@gmail.com>
2026-09-16 00:35:48 +0900

    HomePageのUIを実際のplayerDataから反映

--- lib/presentation/pages/home/components/player_info_system_chip.dart
+++ lib/presentation/pages/home/components/player_info_system_chip.dart
@@ -1,3 +1,6 @@
+import 'package:dereruministic/domain/create_deck_recipe/constants/create_deck_recipe_rules.dart';
+import 'package:dereruministic/domain/player/entities/player.dart';
+import 'package:dereruministic/l10n/app_localizations.dart';
 import 'package:dereruministic/presentation/components/app_card.dart';
 import 'package:dereruministic/presentation/theme/app_color_scheme.dart';
 import 'package:dereruministic/presentation/widgets/ui_flashing_widget.dart';
@@ -6,11 +9,15 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:material_symbols_icons/symbols.dart';
 
 class PlayerInfoSystemChip extends StatelessWidget {
-  const PlayerInfoSystemChip({super.key});
+  const PlayerInfoSystemChip({required this.player, super.key});
+
+  final Player player;
 
   @override
   Widget build(BuildContext context) {
+    final l10n = AppLocalizations.of(context);
     final theme = context.themePalette;
+    final cardsCount = player.deckRecipe.cardsCount;
 
     return AppCard(
       child: Column(
@@ -30,7 +37,10 @@ class PlayerInfoSystemChip extends StatelessWidget {
                 crossAxisAlignment: .start,
                 children: [
                   Text(
-                    'SYSTEM. OK', //TODO(text): l10n
+                    l10n.home_page_user_deck_count(
+                      cardsCount,
+                      CreateDeckRecipeRules.maxDeckCards,
+                    ),
                     style: GoogleFonts.shareTechMono(
                       color: theme.brandSecondary,
                     ),
