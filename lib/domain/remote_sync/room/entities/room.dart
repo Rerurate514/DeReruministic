fa69b4b08ef4270eb0f1e2d06aaffbb01735f1bb
Rerurate514 <rerurateyuto@gmail.com>
2026-09-16 10:20:14 +0900

    roomエンティティからfirebaseの依存を排除

--- lib/domain/remote_sync/room/entities/room.dart
+++ lib/domain/remote_sync/room/entities/room.dart
@@ -1,7 +1,5 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dereruministic/domain/player/converter/player_id_converter.dart';
 import 'package:dereruministic/domain/player/value_objects/player_id.dart';
-import 'package:dereruministic/domain/remote_sync/converter/firebase_timestamp_converter.dart';
 import 'package:dereruministic/domain/remote_sync/converter/room_id_converter.dart';
 import 'package:dereruministic/domain/remote_sync/room/value_objects/room_id.dart';
 import 'package:dereruministic/domain/remote_sync/room/value_objects/room_status.dart';
@@ -16,8 +14,8 @@ sealed class Room with _$Room {
     @RoomIdConverter() required RoomId roomId,
     @PlayerIdConverter() required PlayerId hostPlayerId,
     required RoomStatus status,
-    @FirestoreTimestampConverter() required Timestamp createdAt,
-    @FirestoreTimestampConverter() required Timestamp updatedAt,
+    required DateTime createdAt,
+    required DateTime updatedAt,
     @NullablePlayerIdConverter() PlayerId? guestPlayerId,
   }) = _Room;
 
